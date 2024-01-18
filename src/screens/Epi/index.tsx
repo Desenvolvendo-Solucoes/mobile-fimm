6f639e9fb6863d29379203f7506c6de22da95a10
tfhgzeo <gustavoacm06@gmail.com>
2024-01-18 13:33:03 -0300

    removed clock

--- src/screens/Epi/index.tsx
+++ src/screens/Epi/index.tsx
@@ -5,7 +5,7 @@ import Clock from "../../components/Clock";
 const Epi: React.FC = () => {
   return (
     <View className='flex flex-1 justify-center items-center '>
-      <Clock />
+      <Text>Epi</Text>
     </View>
   )
 }
