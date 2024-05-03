1e7c36bf5b3e43012c942a151de592d0cd554931
MohammedReis <70699636+MohammedReis@users.noreply.github.com>
2024-05-03 08:45:06 -0300

    Feat: adicionando as rotas

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,6 +1,7 @@
 import { createContext, useContext, useEffect, useState } from 'react'
 import axios from 'axios'
 import * as SecureStore from 'expo-secure-store'
+import { string } from 'prop-types';
 
 interface AuthProp {
   authState?: { token: string | null; authenticated: boolean | null }
@@ -10,7 +11,7 @@ interface AuthProp {
 }
 
 const TOKEN_KEY = 'my-jwt'
-export const API_URL = 'http://192.168.1.6:3000'
+export const API_URL = 'https://fimm-api.8corp.com.br'
 const AuthContext = createContext<AuthProp>({})
 
 export const useAuth = () => {
@@ -47,7 +48,6 @@ export const AuthProvider = ({ children }: any) => {
       return { error: true, msg: (e as any).response.data.msg }
     }
   }
-
   const login = async (email: string, senha: string) => {
     try {
       const result = await axios.post(`${API_URL}/auth/login`, null, { params: { email, senha } })
@@ -69,7 +69,6 @@ export const AuthProvider = ({ children }: any) => {
       return { error: true, msg: (e as any).response.data.msg }
     }
   }
-
   const logout = async () => {
     // Deleta o token do storage
     await SecureStore.deleteItemAsync(TOKEN_KEY)
@@ -83,11 +82,364 @@ export const AuthProvider = ({ children }: any) => {
       authenticated: false
     })
   }
+  const createUser = async (email: string, senha: string, nome:string, funcao:string, regiao:string, matricula:string, contrato:string) => {
+    try {
+      const result = await axios.put(`${API_URL}/user/create`, null, { params: { email, senha, nome, funcao, regiao, matricula, contrato } })
+
+      setAuthState({
+        token: result.data.access_token,
+        authenticated: true
+      })
+
+      axios.defaults.headers.common['Authorization'] = `Bearer ${result.data.access_token}`
+
+      await SecureStore.setItemAsync(TOKEN_KEY, result.data.access_token)
+
+      return result
+
+    } catch (e) {
+      console.log(e);
+
+      return { error: true, msg: (e as any).response.data.msg }
+    }
+  }
+  const getUserData = async (email: string) => {
+    try {
+      const result = await axios.get(`${API_URL}/user/getUserData`, { params: { email } })
+
+      setAuthState({
+        token: result.data.access_token,
+        authenticated: true
+      })
+
+      axios.defaults.headers.common['Authorization'] = `Bearer ${result.data.access_token}`
+
+      await SecureStore.setItemAsync(TOKEN_KEY, result.data.access_token)
+
+      return result
+
+    } catch (e) {
+      console.log(e);
+
+      return { error: true, msg: (e as any).response.data.msg }
+    }
+  }
+  const getUserAll = async () => {
+    try {
+      const result = await axios.get(`${API_URL}/user/getAll`)
+
+      setAuthState({
+        token: result.data.access_token,
+        authenticated: true
+      })
+
+      axios.defaults.headers.common['Authorization'] = `Bearer ${result.data.access_token}`
+
+      await SecureStore.setItemAsync(TOKEN_KEY, result.data.access_token)
+
+      return result
+
+    } catch (e) {
+      console.log(e);
+
+      return { error: true, msg: (e as any).response.data.msg }
+    }
+  }
+  const updateUser = async (email: string, senha: string, nome:string, funcao:string, regiao:string, matricula:string, contrato:string) => {
+    try {
+      const result = await axios.post(`${API_URL}/user/update`, null, { params: { email, senha, nome, funcao, regiao, matricula, contrato } })
+
+      setAuthState({
+        token: result.data.access_token,
+        authenticated: true
+      })
+
+      axios.defaults.headers.common['Authorization'] = `Bearer ${result.data.access_token}`
+
+      await SecureStore.setItemAsync(TOKEN_KEY, result.data.access_token)
+
+      return result
+
+    } catch (e) {
+      console.log(e);
+
+      return { error: true, msg: (e as any).response.data.msg }
+    }
+  }
+  const createEpi = async (nome:string, cod:string, imagem:string, problemas:[]) => {
+    try {
+      const result = await axios.put(`${API_URL}/epi/create`, null, { params: { nome, cod, imagem, problemas } })
+
+      setAuthState({
+        token: result.data.access_token,
+        authenticated: true
+      })
+
+      axios.defaults.headers.common['Authorization'] = `Bearer ${result.data.access_token}`
+
+      await SecureStore.setItemAsync(TOKEN_KEY, result.data.access_token)
+
+      return result
+
+    } catch (e) {
+      console.log(e);
+
+      return { error: true, msg: (e as any).response.data.msg }
+    }
+  }
+  const updateEpi = async (nome:string, cod:string, imagem:string, problemas:[]) => {
+    try {
+      const result = await axios.post(`${API_URL}/epi/update`, null, { params: { nome, cod, imagem, problemas } })
+
+      setAuthState({
+        token: result.data.access_token,
+        authenticated: true
+      })
+
+      axios.defaults.headers.common['Authorization'] = `Bearer ${result.data.access_token}`
+
+      await SecureStore.setItemAsync(TOKEN_KEY, result.data.access_token)
+
+      return result
+
+    } catch (e) {
+      console.log(e);
+
+      return { error: true, msg: (e as any).response.data.msg }
+    }
+  }
+  const updateEpiStatus = async (status:string, uid:string) => {
+    try {
+      const result = await axios.post(`${API_URL}/epi/updatestatus`, null, { params: { status, uid } })
+
+      setAuthState({
+        token: result.data.access_token,
+        authenticated: true
+      })
+
+      axios.defaults.headers.common['Authorization'] = `Bearer ${result.data.access_token}`
+
+      await SecureStore.setItemAsync(TOKEN_KEY, result.data.access_token)
+
+      return result
+
+    } catch (e) {
+      console.log(e);
+
+      return { error: true, msg: (e as any).response.data.msg }
+    }
+  }
+  const solicitaEpi = async (nome:string, dataSolicitacao:string, foto:string, problemas:string, uid:string) => {
+    try {
+      const result = await axios.post(`${API_URL}/epi/solicita`, null, { params: { nome, dataSolicitacao, foto, problemas,uid } })
+
+      setAuthState({
+        token: result.data.access_token,
+        authenticated: true
+      })
+
+      axios.defaults.headers.common['Authorization'] = `Bearer ${result.data.access_token}`
+
+      await SecureStore.setItemAsync(TOKEN_KEY, result.data.access_token)
+
+      return result
+
+    } catch (e) {
+      console.log(e);
+
+      return { error: true, msg: (e as any).response.data.msg }
+    }
+  }
+  const getEpiSolicitacoes = async () => {
+    try {
+      const result = await axios.get(`${API_URL}/epi/solicitacoes`,)
+
+      setAuthState({
+        token: result.data.access_token,
+        authenticated: true
+      })
+
+      axios.defaults.headers.common['Authorization'] = `Bearer ${result.data.access_token}`
+
+      await SecureStore.setItemAsync(TOKEN_KEY, result.data.access_token)
+
+      return result
+
+    } catch (e) {
+      console.log(e);
+
+      return { error: true, msg: (e as any).response.data.msg }
+    }
+  }
+  const getEpiCadastrados = async () => {
+    try {
+      const result = await axios.get(`${API_URL}/epi/cadastrados`,)
+
+      setAuthState({
+        token: result.data.access_token,
+        authenticated: true
+      })
+
+      axios.defaults.headers.common['Authorization'] = `Bearer ${result.data.access_token}`
+
+      await SecureStore.setItemAsync(TOKEN_KEY, result.data.access_token)
+
+      return result
+
+    } catch (e) {
+      console.log(e);
+
+      return { error: true, msg: (e as any).response.data.msg }
+    }
+  }
+  const createEquipamento = async (nome:string, cod:string, imagem:string, problemas:[]) => {
+    try {
+      const result = await axios.put(`${API_URL}/equip/create`, null, { params: { nome, cod, imagem, problemas } })
+
+      setAuthState({
+        token: result.data.access_token,
+        authenticated: true
+      })
+
+      axios.defaults.headers.common['Authorization'] = `Bearer ${result.data.access_token}`
+
+      await SecureStore.setItemAsync(TOKEN_KEY, result.data.access_token)
+
+      return result
+
+    } catch (e) {
+      console.log(e);
+
+      return { error: true, msg: (e as any).response.data.msg }
+    }
+  }
+  const solicitaEquipamento = async (nome:string, dataSolicitacao:string, foto:string, problemas:string, uid:string) => {
+    try {
+      const result = await axios.post(`${API_URL}/equip/solicita`, null, { params: { nome, dataSolicitacao, foto, problemas,uid } })
+
+      setAuthState({
+        token: result.data.access_token,
+        authenticated: true
+      })
+
+      axios.defaults.headers.common['Authorization'] = `Bearer ${result.data.access_token}`
+
+      await SecureStore.setItemAsync(TOKEN_KEY, result.data.access_token)
+
+      return result
+
+    } catch (e) {
+      console.log(e);
+
+      return { error: true, msg: (e as any).response.data.msg }
+    }
+  }
+  const updateEquipamentoStatus = async (status:string, uid:string) => {
+    try {
+      const result = await axios.post(`${API_URL}/equip/updatestatus`, null, { params: { status, uid } })
+
+      setAuthState({
+        token: result.data.access_token,
+        authenticated: true
+      })
+
+      axios.defaults.headers.common['Authorization'] = `Bearer ${result.data.access_token}`
+
+      await SecureStore.setItemAsync(TOKEN_KEY, result.data.access_token)
+
+      return result
+
+    } catch (e) {
+      console.log(e);
+
+      return { error: true, msg: (e as any).response.data.msg }
+    }
+  }
+  const getEquipamentoSolicitacoes = async () => {
+    try {
+      const result = await axios.get(`${API_URL}/equip/solicitacoes`,)
+
+      setAuthState({
+        token: result.data.access_token,
+        authenticated: true
+      })
+
+      axios.defaults.headers.common['Authorization'] = `Bearer ${result.data.access_token}`
+
+      await SecureStore.setItemAsync(TOKEN_KEY, result.data.access_token)
+
+      return result
+
+    } catch (e) {
+      console.log(e);
+
+      return { error: true, msg: (e as any).response.data.msg }
+    }
+  }
+  const getEquipamentoCadastrados = async () => {
+    try {
+      const result = await axios.get(`${API_URL}/equip/cadastrados`,)
+
+      setAuthState({
+        token: result.data.access_token,
+        authenticated: true
+      })
+
+      axios.defaults.headers.common['Authorization'] = `Bearer ${result.data.access_token}`
+
+      await SecureStore.setItemAsync(TOKEN_KEY, result.data.access_token)
+
+      return result
+
+    } catch (e) {
+      console.log(e);
+
+      return { error: true, msg: (e as any).response.data.msg }
+    }
+  }
+  const epiMassive = async () => {
+    try {
+      const result = await axios.post(`${API_URL}/epi/massive`,)
+
+      setAuthState({
+        token: result.data.access_token,
+        authenticated: true
+      })
+
+      axios.defaults.headers.common['Authorization'] = `Bearer ${result.data.access_token}`
+
+      await SecureStore.setItemAsync(TOKEN_KEY, result.data.access_token)
+
+      return result
+
+    } catch (e) {
+      console.log(e);
+
+      return { error: true, msg: (e as any).response.data.msg }
+    }
+  }
 
   const value = {
     onRegister: register,
     onLogin: login,
     onLogout: logout,
+    onCreateUser: createUser,
+    onGetUserData: getUserData,
+    onGetUserAll: getUserAll,
+    onUpdateUser: updateUser,
+    onCreateEpi: createEpi,
+    onUpdateEpi: updateEpi,
+    onUpdateEpiStatus: updateEpiStatus,
+    onSolicitaEpi: solicitaEpi,
+    onGetEpiSolicitacoes: getEpiSolicitacoes,
+    onGetEpiCadastrados: getEpiCadastrados,
+    onCreateEquipamento: createEquipamento,
+    onSolicitaEquipamento: solicitaEquipamento,
+    onUpdateEquipamentoStatus: updateEquipamentoStatus,
+    onGetEquipamentoSolicitacoes: getEquipamentoSolicitacoes,
+    onGetEquipamentoCadastrados: getEquipamentoCadastrados,
+    onEpiMassive: epiMassive,
+
     authState: authState,
   }
 
